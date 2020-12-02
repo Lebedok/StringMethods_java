17b43447f00132803acbd91ef46d3e7d99da5203
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-01 22:57:38 -0600

    Add files via upload

---
+++ TrimMethod.java
@@ -0,0 +1,11 @@
+package StringExample;
+
+public class TrimMethod {
+
+    public static void main(String[] args) {
+
+        String str = "   Los Angeles   ";
+        System.out.println(str.trim());
+
+    }
+}
