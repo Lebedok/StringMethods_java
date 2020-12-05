736b07265aa5774429f5e6c6228f3287d495347d
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-04 21:53:09 -0600

    Add files via upload

---
+++ RplaceMethod.java
@@ -0,0 +1,23 @@
+package StringExample;
+
+public class RplaceMethod {
+
+    public static void main(String[] args) {
+
+        String car = "honda";
+        System.out.println(car.replace("h", "H"));
+
+        car = car.replace("honda", "toyota");  //toyota
+        System.out.println(car);
+
+        System.out.println(car.replace("o", "1"));  //t1y1ta
+
+
+
+
+
+
+
+
+    }
+}
