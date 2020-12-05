67d39d21036373629f6ab7263eed5b3d7cc7a679
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-04 21:51:29 -0600

    Add files via upload

---
+++ EndWithMethod.java
@@ -0,0 +1,16 @@
+package StringExample;
+
+public class EndWithMethod {
+
+    public static void main(String[] args) {
+
+        String brand = "microsoft";
+        boolean result = brand.endsWith("oft");
+        System.out.println(result);
+
+        String name = "Nargiza";
+        boolean result2 = name.endsWith("iza");
+        System.out.println(result2);
+
+    }
+}
