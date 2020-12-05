5f04398e03a24bcb96037a5cfe5fa6d61997db42
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-04 21:52:28 -0600

    Add files via upload

---
+++ StartWithMethod.java
@@ -0,0 +1,13 @@
+package StringExample;
+
+public class StartWithMethod {
+
+    public static void main(String[] args) {
+
+        String city = "new York";
+        System.out.println(city.startsWith("Nice"));
+
+        boolean result = city.startsWith("icro");
+        System.out.println(result);
+    }
+}
