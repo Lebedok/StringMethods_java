5b607337e5ad5c8361607c49ee81e26bfe116ccc
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-05 22:47:49 -0600

    Add files via upload

---
+++ EqualMethod.java
@@ -0,0 +1,14 @@
+package StringExample;
+
+public class EqualMethod {
+
+    public static void main(String[] args) {
+
+        String name = "microsoft";
+        boolean result = name.equals("soft");
+        boolean result2 = name.equals("microsoft");
+        System.out.println(result);
+        System.out.println(result2);
+
+    }
+}
