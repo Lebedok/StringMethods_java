27841584b6c55c4968b3c84e90a14e29e2892dc4
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-05 22:43:45 -0600

    Add files via upload

---
+++ NullPointerException.java
@@ -0,0 +1,11 @@
+package StringExample;
+
+public class NullPointerException {
+
+    public static void main(String[] args) {
+
+        String test = null;
+        System.out.println(test.length());  // if you assign null there is no working any method
+
+    }
+}
